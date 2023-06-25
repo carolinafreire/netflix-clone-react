5e526546668005f8a0d64722ddaf0b1e26d25a5d
Carolina Freire <68876784+carolinafreire@users.noreply.github.com>
2023-06-24 21:55:44 -0300

    final app

--- src/App.js
+++ src/App.js
@@ -57,6 +57,11 @@ export default () => {
         Clone Netflix - Direitos de imagem para Netflix<br/>
         Dados pegos do site Themoviedb.org
       </footer>
+      {movieList.length <= 0 &&
+          <div className="loading"> 
+            <img src="https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif" alt="Carregando" />
+            </div>
+        }
     </div>
   );
 }
